f27f68af02a6f64828396996a31c73605095fb89
Ikwukala-Mbas Ifechimenin <mbas750@gmail.com>
2023-10-03 10:52:14 +0100

    convert to roman numeral

---
+++ roman-numeral-converter.js
@@ -0,0 +1,17 @@
+function convertToRoman(num) {
+  const romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  const arabicNumerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+  let result = '';
+
+  for (let i = 0; i < arabicNumerals.length; i++){
+    while (num >= arabicNumerals[i]){
+      result += romanNumerals[i]
+      num -= arabicNumerals[i]
+    }
+  };
+
+ return result;
+}
+
+//console.log(convertToRoman(36));
